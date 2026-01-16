5521c3fd2cce4f999ed426feed0098f5c2f294d5
ilyushkin_delmic <ilyushkin@delmic.com>
2026-01-16 15:51:08 +0000

    Run flow 019bc74c-9734-7dd3-94e5-5a5ee9d0a087

--- fib_acquisition.py
+++ fib_acquisition.py
@@ -1,21 +1,29 @@
 import balthazar as blt
 from tescanautomation import Automation
+from tescanautomation.Common import Bpp
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 microscope_ip = blt.params['ip_address']
 
 session = Automation(microscope_ip)  # default port 8300
 session.FIB.Detector.Set(0, 'SE')
-session.FIB.Detector.AutoSignal(0)
-status = session.FIB.Beam.GetStatus()
-if status != Automation.FIB.Beam.Status.On:
-    session.FIB.Beam.On()
-    # Wait for beam transition
-    while session.FIB.Beam.GetStatus() == Automation.FIB.Beam.Status.OnOffInProgress:
-        time.sleep(0.1)
-print('Original Current:', session.FIB.Beam.GetCurrent(), ' pA')
-print('Original Voltage:', session.FIB.Beam.GetVoltage(), ' V')
-session.FIB.Beam.SetCurrent(10)
-session.FIB.Beam.SetVoltage(30000)
-print('New Current:', session.FIB.Beam.GetCurrent(), ' pA')
-print('New Voltage:', session.FIB.Beam.GetVoltage(), ' V')
+# session.FIB.Detector.AutoSignal(0)
+# status = session.FIB.Beam.GetStatus()
+# if status != Automation.FIB.Beam.Status.On:
+#     session.FIB.Beam.On()
+#     # Wait for beam transition
+#     while session.FIB.Beam.GetStatus() == Automation.FIB.Beam.Status.OnOffInProgress:
+#         time.sleep(0.1)
+# print('Original Current:', session.FIB.Beam.GetCurrent(), ' pA')
+# print('Original Voltage:', session.FIB.Beam.GetVoltage(), ' V')
+# session.FIB.Beam.SetCurrent(10)
+# session.FIB.Beam.SetVoltage(30000)
+# print('New Current:', session.FIB.Beam.GetCurrent(), ' pA')
+# print('New Voltage:', session.FIB.Beam.GetVoltage(), ' V')
+
+w, h, dwell = 512, 512, 1000
+doc = session.FIB.Scan.AcquireImage("SE", Bpp.Grayscale_16_bit, w, h, dwell)
+
+doc.Image.show()
