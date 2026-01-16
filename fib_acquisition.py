761cc9e2305cd6b93c4ca29b4f5bc658d5c59056
ilyushkin_delmic <ilyushkin@delmic.com>
2026-01-16 15:22:08 +0000

    Run flow 019bc74c-9734-7dd3-94e5-5a5ee9d0a087

--- fib_acquisition.py
+++ fib_acquisition.py
@@ -1,6 +1,21 @@
+import balthazar as blt
 from tescanautomation import Automation
 
 
+microscope_ip = blt.params['ip_address']
 
-
-session = Automation(ip_address, 8300)
+session = Automation(microscope_ip)  # default port 8300
+session.FIB.Detector.Set(0, 'SE')
+session.FIB.Detector.AutoSignal(0)
+status = session.FIB.Beam.GetStatus()
+if status != Automation.FIB.Beam.Status.On:
+    session.FIB.Beam.On()
+    # Wait for beam transition
+    while session.FIB.Beam.GetStatus() == Automation.FIB.Beam.Status.OnOffInProgress:
+        time.sleep(0.1)
+print('Original Current:', session.FIB.Beam.GetCurrent(), ' pA')
+print('Original Voltage:', session.FIB.Beam.GetVoltage(), ' V')
+session.FIB.Beam.SetCurrent(10)
+session.FIB.Beam.SetVoltage(30000)
+print('New Current:', session.FIB.Beam.GetCurrent(), ' pA')
+print('New Voltage:', session.FIB.Beam.GetVoltage(), ' V')
